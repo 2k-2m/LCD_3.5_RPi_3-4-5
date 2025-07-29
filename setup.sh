f54e64ea6dc65ee7faccdb37fc47e2e50c56d837
2k-2m <kalebmaury@gmail.com>
2025-07-29 14:25:13 -0600

    Actualizado script para forzar X11 y desactivar Wayland

--- setup.sh
+++ setup.sh
@@ -9,10 +9,11 @@ fi
 
 # Habilitar SPI y X11 (Wayland desactivado por compatibilidad con xinput_calibrator)
 raspi-config nonint do_spi 0
-raspi-config nonint do_wayland 0
+#raspi-config nonint do_wayland 0
 
 # Modificar /boot/firmware/config.txt
 sed -i 's/^dtoverlay=vc4-kms-v3d/#dtoverlay=vc4-kms-v3d/' /boot/firmware/config.txt
+sed -i 's/^dtoverlay=vc4-fkms-v3d/#dtoverlay=vc4-fkms-v3d/' /boot/firmware/config.txt
 
 if ! grep -q "dtoverlay=piscreen" /boot/firmware/config.txt; then
   echo "dtoverlay=piscreen,speed=18000000,drm" >> /boot/firmware/config.txt
